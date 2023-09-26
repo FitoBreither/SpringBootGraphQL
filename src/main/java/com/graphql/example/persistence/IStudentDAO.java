d6b9fcc66899a4ed5b9d78d041c3ecf69896dbb3
FitoBreither <a_breither@hotmail.com>
2023-09-25 22:59:01 -0300

    first commit

---
+++ src/main/java/com/graphql/example/persistence/IStudentDAO.java
@@ -0,0 +1,9 @@
+package com.graphql.example.persistence;
+
+import com.graphql.example.entities.Student;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IStudentDAO extends CrudRepository<Student, Long> {
+
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO courses (name, category, teacher) VALUES ('Matematicas', 'Ciencias exactas', "Juan Diego Osa");
+INSERT INTO courses (name, category, teacher) VALUES ('Biologia', 'Ciencias', "Leandro Jaramillo");
+INSERT INTO courses (name, category, teacher) VALUES ('Programacion', 'Tecnologia', "Neiro Figueroa");
+
+INSERT INTO students (name, last_name, age, course_id) VALUES ('John', 'Serna', '26', 1);
+INSERT INTO students (name, last_name, age, course_id) VALUES ('Andrea', 'Calle', '21', 2);
+INSERT INTO students (name, last_name, age, course_id) VALUES ('Anyi', 'Hoyos', '22', 1);
+INSERT INTO students (name, last_name, age, course_id) VALUES ('Santiago', 'Perez', '24', 1);
